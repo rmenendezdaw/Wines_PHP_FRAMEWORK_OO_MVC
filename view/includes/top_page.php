ba935e205576b81410e7917b874c0d62d55c18c7
rmenendezdaw <rmenendeziestacio@gmail.com>
2020-04-26 04:22:03 +0200

    Change API

--- view/includes/top_page.php
+++ view/includes/top_page.php
@@ -19,7 +19,7 @@
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAo2yml1SJDD0fxGJxA3NJDin0486fTkRY&callback=initMap"></script> -->
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap"></script> -->
 
   <script src="view/includes/api_keys/api_key.js"></script>
 
